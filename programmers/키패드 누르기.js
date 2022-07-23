b986af1c7caa4887cfcdd95946f9cc5cc3806c7e
gomgun <zxolqs@gmail.com>
2022-07-23 17:48:04 +0900

    init

---
+++ programmers/키패드 누르기.js
@@ -0,0 +1,54 @@
+function solution(numbers, hand) {
+  // 1  2  3
+  // 4  5  6
+  // 7  8  9
+  // 10 11 12
+
+  let leftNum = 10;
+  let rightNum = 12;
+
+  const answer = numbers.map((num) => {
+    if (num === 0) num = 11;
+    if (num % 3 === 1) return left(num);
+    else if (num % 3 === 0) return right(num);
+    else {
+      const leftDistance = calcDistance(
+        calcCoordinate(num),
+        calcCoordinate(leftNum)
+      );
+      const rightDistance = calcDistance(
+        calcCoordinate(num),
+        calcCoordinate(rightNum)
+      );
+      console.log(num, leftNum, rightNum, leftDistance, rightDistance);
+      if (leftDistance < rightDistance) return left(num);
+      else if (rightDistance < leftDistance) return right(num);
+      else {
+        return hand === "right" ? right(num) : left(num);
+      }
+    }
+  });
+
+  function left(num) {
+    leftNum = num;
+    return "L";
+  }
+
+  function right(num) {
+    rightNum = num;
+    return "R";
+  }
+
+  return answer.join("");
+}
+
+function calcCoordinate(num) {
+  return [Math.floor((num - 1) / 3), (num - 1) % 3];
+}
+
+function calcDistance(curNumCord, tarNumCord) {
+  return (
+    Math.abs(curNumCord[0] - tarNumCord[0]) +
+    Math.abs(curNumCord[1] - tarNumCord[1])
+  );
+}
