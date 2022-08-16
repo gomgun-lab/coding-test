fd9c6eb041676b3b4561006e8395578e93d28d02
gomgun <zxolqs@gmail.com>
2022-08-17 00:28:33 +0900

    같은 숫자는 싫어

---
+++ programmers/lv-1/같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
+
+function solution(arr) {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      continue;
+    }
+    answer.push(arr[i]);
+  }
+
+  return answer;
+}
