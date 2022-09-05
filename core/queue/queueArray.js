8a3bd0c28f1175bdc778a16ac4724e8e5be54b47
gomgun <zxolqs@gmail.com>
2022-09-05 22:19:02 +0900

    queue

---
+++ core/queue/queueArray.js
@@ -0,0 +1,42 @@
+// rear -> put
+// front -> get
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+const q = new Queue();
+
+q.enqueue(3);
+
+console.log(q);
+
+q.dequeue();
+
+console.log(q);
+
+console.log(q.peek());
+console.log(q.szie());
