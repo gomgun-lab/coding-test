5bd644e0860c2213966f195ea18872750d7e0bb6
gomgun <zxolqs@gmail.com>
2022-09-30 03:54:58 +0900

    2217

---
+++ boj/2217.js
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/13305
+
+const { array } = require("./fs/multiLine");
+
+const n = array[0];
+const lopeList = [];
+
+for (let i = 1; i <= n; i++) {
+  lopeList.push(parseInt(array[i]));
+}
+
+let maxWeight = 0;
+let curWeights = 50000;
+
+lopeList.sort((a, b) => b - a);
+
+for (let i = 0; i < n; i++) {
+  if (curWeights > lopeList[i]) {
+    curWeights = lopeList[i];
+  }
+  maxWeight = Math.max(curWeights * (i + 1), maxWeight);
+}
+
+console.log(maxWeight);
