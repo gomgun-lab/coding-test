147ab88315b11cc7e83b96dcd5b890beea46e7ff
Choi Hong Gyu <zxolqs@gmail.com>
2022-10-20 01:25:55 +0900

    12865

---
+++ boj/12865.js
@@ -0,0 +1,18 @@
+const { array } = require("./fs/multiLine");
+
+const [n, k] = array[0].map(Number);
+const items = array.slice(1).map((item) => item.map(Number));
+const dp = Array.from(Array(n + 1), () => Array(k + 1).fill(0));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= k; j++) {
+    const [w, v] = items[i - 1];
+    if (j >= w) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+    } else {
+      dp[i][j] = dp[i - 1][j];
+    }
+  }
+}
+
+console.log(dp[n][k]);
