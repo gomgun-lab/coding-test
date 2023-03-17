eb8a9f89f563948217c63af9ce9fb30fe2a15a75
Choi Hong Gyu <zxolqs@gmail.com>
2023-03-17 16:09:05 +0900

    fix fs/input.js
    - 운영체제에 따른 공백 처리 대응

--- fs/input.js
+++ fs/input.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const fs = require("fs");
+
 const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : `${path.dirname(__filename)}/input.txt`;
-let input = fs.readFileSync(filePath).toString().split("\r\n");
+
+let temp = fs.readFileSync(filePath).toString();
+
+const input =
+  process.platform === "linux" ? temp.split("\n") : temp.split("\r\n");
 
 module.exports = input;
