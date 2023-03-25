8ba395db69310c4645926e1c710eaa2774a145f0
Choi Hong Gyu <zxolqs@gmail.com>
2023-03-26 01:48:27 +0900

    11724

---
+++ boj/dfs/11724.js
@@ -0,0 +1,42 @@
+const input = require("../../fs/input");
+
+const [N, M] = input[0].split(" ").map(Number);
+const edges = input.slice(1).map((line) => line.split(" ").map(Number));
+
+const graph = Array.from(Array(N + 1), () => []);
+const visited = Array(N + 1).fill(false);
+
+console.log(N, M);
+console.log(edges);
+console.log(graph);
+console.log(visited);
+
+function solve(N, M, edges) {
+  let components = 0;
+
+  for (const [u, v] of edges) {
+    graph[u].push(v);
+    graph[v].push(u);
+  }
+
+  for (let i = 1; i < N + 1; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      components++;
+    }
+  }
+
+  return components;
+}
+
+function dfs(node) {
+  visited[node] = true;
+
+  for (const neighbor of graph[node]) {
+    if (!visited[neighbor]) {
+      dfs(neighbor);
+    }
+  }
+}
+
+console.log(solve(N, M, edges));
